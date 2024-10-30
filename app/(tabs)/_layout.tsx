967deb94cf0ac4e09084c494773e8fa676283d33
Ramit Goolry <ramitgoolry@gmail.com>
2024-10-29 22:34:33 -0700

    Remove index and explore screens, they were not needed

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -15,20 +15,11 @@ export default function TabLayout() {
         headerShown: false,
       }}>
       <Tabs.Screen
-        name="index"
+        name="draw"
         options={{
-          title: 'Home',
+          title: 'Draw',
           tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
+            <TabBarIcon name={focused ? 'brush' : 'brush-outline'} color={color} />
           ),
         }}
       />
