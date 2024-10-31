7df11038d1c64dca7a0b4579a7a31545aa573a6c
Ramit Goolry <ramitgoolry@gmail.com>
2024-10-30 22:59:58 -0700

    Basic PDF support

--- app/(tabs)/draw.tsx
+++ app/(tabs)/draw.tsx
@@ -1,17 +1,22 @@
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { Canvas, Path } from "@shopify/react-native-skia";
 import { GestureDetector } from "react-native-gesture-handler";
-import { StyleSheet, View, Text } from "react-native";
-import React, { useMemo } from "react";
+import { StyleSheet, View, Text, useWindowDimensions } from "react-native";
+import React, { useMemo, useState } from "react";
 import { ThemedView } from '../../components/ThemedView';
 import { useThemeColor } from '../../hooks/useThemeColor';
 import { useDrawing } from "@/hooks/useDrawing";
 import { Point } from "@/types/drawing";
+import Pdf from 'react-native-pdf';
 
 export default function DrawScreen() {
+  const { width, height } = useWindowDimensions();
   const { paths, gesture } = useDrawing();
 
-  const textColor = useThemeColor({}, 'text');
+  const [pdfSource, _setPdfSource] = useState({
+    uri: 'https://pdfobject.com/pdf/sample.pdf',
+    cache: true,
+  });
 
   const pathToSvg = (points: Point[]) => {
     if (points.length < 1) return "";
@@ -38,20 +43,29 @@ export default function DrawScreen() {
     )), [paths]);
 
   return (
-    <ThemedView style={styles.container}>
-      <GestureHandlerRootView style={styles.container}>
+    <GestureHandlerRootView style={styles.container}>
+      <ThemedView style={styles.container}>
+        {/* PDF Layer */}
+        <View style={styles.pdfContainer}>
+          <Pdf
+            source={pdfSource}
+            style={[styles.pdf, { width, height }]}
+            onLoadComplete={(numberOfPages, _filePath) => {
+              console.log(`Loaded ${numberOfPages} pages`);
+            }}
+            onError={(error) => {
+              console.log(error);
+            }}
+          />
+        </View>
+        {/* Gesture Layer */}
         <GestureDetector gesture={gesture}>
           <Canvas style={{ flex: 1 }}>
             {renderedPaths}
           </Canvas>
         </GestureDetector>
-        <View style={styles.indicator}>
-          <Text style={{ color: textColor }}>
-            Input: Any (Enabled with two fingers)
-          </Text>
-        </View>
-      </GestureHandlerRootView>
-    </ThemedView>
+      </ThemedView>
+    </GestureHandlerRootView>
   );
 }
 
@@ -66,5 +80,13 @@ const styles = StyleSheet.create({
     padding: 10,
     backgroundColor: "rgba(255, 255, 255, 0.8)",
     borderRadius: 5,
-  }
+  },
+  pdfContainer: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+  },
+  pdf: {
+    flex: 1,
+  },
 });
